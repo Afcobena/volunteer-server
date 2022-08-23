03665207f50da14959d2b9ddc90ee7c1b6ba6117
Adrian F. Cobena <adrianfcobena@gmail.com>
2022-08-23 18:41:58 +0200

    Modificacion del Profile.

--- routes/profile.routes.js
+++ routes/profile.routes.js
@@ -8,13 +8,14 @@ const isAuthenticated = require("../middlewares/isAuthenticated");
 router.get("/", isAuthenticated ,async (req, res, next) => {
 
     const userId = req.payload._id
-    
-    console.log("ESTE ES EL ID", userId)
 
     try {
         const userProposals = await Proposal.find({owner: userId})
+        /* const userCollabs = await Collaborate.find({owner: userId}) */
         console.log("ESTA COSA ES PROFILE", userProposals)
         res.json(userProposals)
+        /* res.json({userProposals, userCollabs}) */
+
         
     } catch (error) {
         next(error)
@@ -22,14 +23,30 @@ router.get("/", isAuthenticated ,async (req, res, next) => {
 })
 
 
-/* router.get("/:id", async (req, res, next) => {
+/* router.get("/", isAuthenticated ,async (req, res, next) => {
 
-    const {id} = req.params
+    const userId = req.payload._id
+    console.log("QUE ES", userID)
     
-    console.log("ESTE ES EL ID", id)
+    try {
+        const userCollabs = await Collaborate.find({owner: userId})
+        console.log("ESTA COSA ES PROFILE", userCollabs)
+        res.json(userCollabs)
+        
+    } catch (error) {
+        next(error)
+    }
+}) */
+
+
+/* router.get("/", isAuthenticated ,async (req, res, next) => {
+
+    const userId = req.payload._id
+    
+    console.log("ESTE ES EL ID", userId)
 
     try {
-        const userProposals = await Proposal.find({owner: id})
+        const userProposals = await Proposal.find({owner: userId})
         console.log("ESTA COSA ES PROFILE", userProposals)
         res.json(userProposals)
         
