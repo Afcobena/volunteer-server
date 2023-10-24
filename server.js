61da25f4133c9b62d7be9cb8ee8981051044550f
Adrian F. Cobena <afcobena.energy@gmail.com>
2023-10-24 22:17:47 +0200

    deploy test 2

--- server.js
+++ server.js
@@ -1,8 +1,10 @@
 const app = require("./app");
+const http = require('http'); 
+const server = http.createServer(app);
 
 // ℹ️ Sets the PORT for our app to have access to it. If no env has been set, we hard code it to 3000
-const PORT = process.env.PORT || 5005;
+const PORT = process.env.PORT;
 
-app.listen(PORT, () => {
+server.listen(PORT, () => {
   console.log(`Server listening on port http://localhost:${PORT}`);
 });
