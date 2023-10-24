a221b2c849f96636eeb9f56e0dba38b658b1c925
Adrian F. Cobena <afcobena.energy@gmail.com>
2023-10-24 22:54:39 +0200

    deploy test 5

--- config/index.js
+++ config/index.js
@@ -1,83 +1,12 @@
-// We reuse this import in order to have access to the `body` property in requests
 const express = require("express");
-
-// ℹ️ Responsible for the messages you see in the terminal as requests are coming in
-// https://www.npmjs.com/package/morgan
 const logger = require("morgan");
-
-// ℹ️ Needed when we deal with cookies (we will when dealing with authentication)
-// https://www.npmjs.com/package/cookie-parser
 const cookieParser = require("cookie-parser");
-
-// ℹ️ Needed to accept from requests from 'the outside'. CORS stands for cross origin resource sharing
-// unless the request if from the same domain, by default express wont accept POST requests
 const cors = require("cors");
-
-const customCors = (req, res, next) => {
-  res.setHeader('Access-Control-Allow-Origin', 'http://localhost:3000');
-  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
-  res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
-  res.setHeader('Access-Control-Allow-Credentials', 'true');
-  next();
-};
-
-// Middleware configuration
 module.exports = (app) => {
-  // Because this is a server that will accept requests from outside and it will be hosted ona server with a `proxy`, express needs to know that it should trust that setting.
-  // Services like heroku use something called a proxy and you need to add this to your server
   app.set("trust proxy", 1);
-
-  // controls a very specific header to pass headers from the frontend
-  app.use(customCors);
-
-  // In development environment the app logs
+  app.use(cors({ credentials: true, origin: process.env.ORIGIN, }));
   app.use(logger("dev"));
-
-  // To have access to `body` property in the request
   app.use(express.json());
   app.use(express.urlencoded({ extended: false }));
   app.use(cookieParser());
 };
-
-
-// const express = require("express");
-// const logger = require("morgan");
-// const cookieParser = require("cookie-parser");
-// const cors = require("cors");
-// /* const origin =  process.env.ORIGIN */
-
-// module.exports = (app) => {
-//   app.set("trust proxy", 1);
-
-//   // Middleware para establecer las cabeceras CORS
-//   app.use(function (req, res, next) {
-//     // Website you wish to allow to connect
-//     res.setHeader("Access-Control-Allow-Origin", "http://localhost:3000"); // Cambiado a tu origen permitido
-
-//     // Request methods you wish to allow
-//     res.setHeader(
-//       "Access-Control-Allow-Methods",
-//       "GET, POST, OPTIONS, PUT, PATCH, DELETE"
-//     );
-
-//     // Request headers you wish to allow
-//     res.setHeader(
-//       "Access-Control-Allow-Headers",
-//       "X-Requested-With,content-type"
-//     );
-
-//     // Set to true if you need the website to include cookies in the requests sent
-//     // to the API (e.g. in case you use sessions)
-//     res.setHeader("Access-Control-Allow-Credentials", true);
-
-//     // Pass to next layer of middleware
-//     next();
-//   });
-
-//   app.use(logger("dev"));
-//   app.use(express.json());
-//   app.use(express.urlencoded({ extended: false }));
-//   app.use(cookieParser());
-
-//   // ... Definición de tus rutas aquí
-// };
